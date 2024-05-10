72c344ebb4cce4c00853e5151e78a1b11c0c9e65
AnDreV133 <dmitrijkubik@gmail.com>
2024-05-10 12:44:50 +0300

    added test for api

---
+++ app/src/test/java/com/example/pokemonhandbook/ApiTest.kt
@@ -0,0 +1,33 @@
+package com.example.pokemonhandbook
+
+import com.example.pokemonhandbook.api.PokemonApiHandler
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ApiTest {
+    @Test
+    fun getPokemon(): Unit = runBlocking {
+        val p = async {
+            PokemonApiHandler.get(1)
+        }.await()
+
+        assertEquals(p?.name, "bulbasaur")
+        assertEquals(
+            p?.sprites?.frontDefault,
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+        )
+    }
+
+    @Test
+    fun getPokemonList(): Unit = runBlocking {
+        val p = async {
+            PokemonApiHandler.getByDiapason(1, 10)
+        }.await()
+
+        assertEquals(p?.get(3)?.name, "charmeleon")
+        assertEquals(p?.get(5)?.name, "squirtle")
+    }
+}
